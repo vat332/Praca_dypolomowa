d3b60b544dbeab1ab8b1671708c49d31c2af1115
vat332 <oporowymakus21@gmail.com>
2022-11-07 16:11:13 +0100

    Add first models and view in admin dashboard

--- Aplikacja/backend/Library/models.py
+++ Aplikacja/backend/Library/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    page = models.IntegerField()
+    publisher = models.CharField(max_length=255)
