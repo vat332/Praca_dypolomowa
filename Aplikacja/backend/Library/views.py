41ddab677f72c2c061324eaf8cdc03bf9ef3c281
vat332 <oporowymakus21@gmail.com>
2022-11-28 21:40:06 +0100

    Add view for endpoints

--- Aplikacja/backend/Library/views.py
+++ Aplikacja/backend/Library/views.py
@@ -1,8 +1,12 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Book
-from .serializers import BookSerializer
+from .models import Book, Customer, Author, Rental ,Language
+from .serializers import BookSerializer, CustomerSerializer, AuthorSerializer, RentalSerializer, LanguageSerializer
+
+
+# For Book
+
 
 @api_view(['GET'])
 def book_list(request):
@@ -33,4 +37,142 @@ def book_detail(request, pk):
 
     elif request.method == 'DELETE':
         book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# For Customer
+
+@api_view(['GET'])
+def customer_list(request):
+    if request.method == 'GET':
+        customers = Customer.objects.all()
+        serializer = CustomerSerializer(customers, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def customer_detail(request, pk):
+    try:
+        customer = Customer.objects.get(pk=pk)
+    except Customer.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        customer = Customer.objects.get(pk=pk)
+        serializer = CustomerSerializer(customer)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CustomerSerializer(customer, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        customer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# For Author
+
+@api_view(['GET'])
+def author_list(request):
+    if request.method == 'GET':
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def author_detail(request, pk):
+    try:
+        author = Author.objects.get(pk=pk)
+    except Author.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        author = Author.objects.get(pk=pk)
+        serializer = AuthorSerializer(author)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = AuthorSerializer(author, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# For Rental
+
+@api_view(['GET'])
+def rental_list(request):
+    if request.method == 'GET':
+        rentals = Rental.objects.all()
+        serializer = RentalSerializer(rentals, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def rental_detail(request, pk):
+    try:
+        rental = Rental.objects.get(pk=pk)
+    except Rental.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        rental = Rental.objects.get(pk=pk)
+        serializer = RentalSerializer(rental)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = RentalSerializer(rental, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        rental.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+#For Language
+
+
+
+@api_view(['GET'])
+def language_list(request):
+    if request.method == 'GET':
+        languages = Language.objects.all()
+        serializer = LanguageSerializer(languages, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def language_detail(request, pk):
+    try:
+        language = Language.objects.get(pk=pk)
+    except Language.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        language = Language.objects.get(pk=pk)
+        serializer = RentalSerializer(language)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = LanguageSerializer(language, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        language.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
