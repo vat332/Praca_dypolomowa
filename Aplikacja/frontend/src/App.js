6b5f858120800368cb97b45e9d6ee5a3f67d578b
vat332 <oporowymakus21@gmail.com>
2023-01-05 13:23:32 +0100

    Revert

---
+++ Aplikacja/frontend/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "bulma/css/bulma.css";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          <div className="buttons">
+            <button className="button is-primary">Primary</button>
+            <button className="button is-link">Link</button>
+          </div>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
